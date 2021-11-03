7b4b666e3a6909884edcaa8268b60a98a57fa040
Don Moses <githinjidon@gmail.com>
2021-11-03 12:27:15 +0300

    register models in admin.py

--- watch/admin.py
+++ watch/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
+from .models import  Post, Profile, Neighborhood, Business
 
-# Register your models here.
+
+admin.site.register(Business)
+admin.site.register(Neighborhood)
+admin.site.register(Post)
+admin.site.register(Profile)
+
