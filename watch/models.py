3849de23182f1f458e434d71223498d2cb5e4994
Don Moses <githinjidon@gmail.com>
2021-11-03 11:28:25 +0300

    add function to create profile linked to user

--- watch/models.py
+++ watch/models.py
@@ -1,5 +1,7 @@
 from django.db import models
 from django.contrib.auth.models import User
+from django.db.models.signals import post_save
+from django.dispatch import receiver
 
 # Create your models here.
 class Neighborhood(models.Model):
@@ -39,10 +41,20 @@ class Profile(models.Model):
     def __str__(self):
         return self.username
 
+# Create Profile when creating a User
+@receiver(post_save, sender=User)
+def create_user_profile(sender, instance, created, **kwargs):
+    if created:
+        Profile.objects.create(user=instance)
+
+# Save Profile when saving a User
+@receiver(post_save, sender=User)
+def save_user_profile(sender, instance, **kwargs):
+    instance.profile.save()
+
 class Post(models.Model):
     post = models.TextField()
     username = models.ForeignKey(User,on_delete=models.CASCADE)
     neighbourhood= models.ForeignKey(Neighborhood,on_delete=models.CASCADE)
     post_date = models.DateTimeField(auto_now_add=True)
 
-    
