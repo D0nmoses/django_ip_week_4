12b8852272af060bd450bcd2d56deb7c87e5abbc
Don Moses <githinjidon@gmail.com>
2021-11-03 14:22:41 +0300

    add static settings to url.py

--- watch/urls.py
+++ watch/urls.py
@@ -1,5 +1,7 @@
 from django.urls import path,re_path
 from . import views
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     path("",views.home,name='home'),
@@ -8,3 +10,6 @@ urlpatterns = [
     path("business",views.business, name="business"),
     path("business/search_results", views.search_results, name="search_results"),
 ]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
